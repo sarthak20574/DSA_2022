e95c92f1d549ba9f98fb83c6a559e9d290d21e17
sarthak20574 <sarthak20574@iiitd.ac.in>
2022-05-03 21:55:46 +0530

    committed on 3 May 2022

---
+++ src/com/company/BestTimetoBuyandSellStock.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class BestTimetoBuyandSellStock {
+
+    /*
+      //121. Best Time to Buy and Sell Stock
+    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
+     int maxProfit(vector<int>& prices) {
+
+        int minn= INT_MAX;
+        int ans=0;
+        int n= prices.size();
+
+
+        for( int i=0; i<n ;i++){
+            if( minn> prices[i]) minn= prices[i];
+
+            ans= max( ans, prices[i]- minn);
+        }
+
+        return ans;
+    }
+ */
+}
