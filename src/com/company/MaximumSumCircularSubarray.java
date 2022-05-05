cb4b5ba15f2b1d9cda03fc564564ea7d95d3511b
sarthak20574 <sarthak20574@iiitd.ac.in>
2022-05-05 12:37:30 +0530

    committed on 5 May 2022

---
+++ src/com/company/MaximumSumCircularSubarray.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class MaximumSumCircularSubarray {
+    //https://www.youtube.com/watch?v=Q1TYVUEr-wY
+
+    /*
+     int maxSubarraySumCircular(vector<int>& A) {
+        int total = 0, maxSum = A[0], curMax = 0, minSum = A[0], curMin = 0;
+        for (int& a : A) {
+            curMax = max(curMax + a, a);
+            maxSum = max(maxSum, curMax);
+            curMin = min(curMin + a, a);
+            minSum = min(minSum, curMin);
+            total += a;
+        }
+        return maxSum > 0 ? max(maxSum, total - minSum) : maxSum;
+    }
+     */
+}
