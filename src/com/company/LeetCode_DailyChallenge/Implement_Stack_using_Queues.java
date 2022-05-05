d085ac1332d5fb7773d7c723ac1de0b0e9cdea5d
sarthak20574 <sarthak20574@iiitd.ac.in>
2022-05-06 00:26:49 +0530

    committed on 6 May 2022

---
+++ src/com/company/LeetCode_DailyChallenge/Implement_Stack_using_Queues.java
@@ -0,0 +1,41 @@
+package com.company.LeetCode_DailyChallenge;
+
+public class Implement_Stack_using_Queues {
+    //225. Implement Stack using Queues
+    //https://leetcode.com/problems/implement-stack-using-queues/
+
+    /*
+      class MyStack {
+public:
+queue<int> stack1;
+    MyStack() {
+
+    }
+
+    void push(int x) {
+
+        stack1.push(x);
+
+        for( int i=0.; i< stack1.size()-1; i++){
+            stack1.push(stack1.front());
+            stack1.pop();
+        }
+    }
+
+    int pop() {
+        int x= stack1.front();
+         stack1.pop();
+        return x;
+    }
+
+    int top() {
+        return stack1.front();
+    }
+
+    bool empty() {
+        return stack1.empty();
+    }
+};
+
+     */
+}
