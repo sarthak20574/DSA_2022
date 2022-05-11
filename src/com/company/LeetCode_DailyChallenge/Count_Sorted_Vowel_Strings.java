47dfbb393677ddbc1abb028d7a4c3c614cda5132
sarthak20574 <sarthak20574@iiitd.ac.in>
2022-05-11 11:29:57 +0530

    committed on 11 May 2022

---
+++ src/com/company/LeetCode_DailyChallenge/Count_Sorted_Vowel_Strings.java
@@ -0,0 +1,28 @@
+package com.company.LeetCode_DailyChallenge;
+
+public class Count_Sorted_Vowel_Strings {
+
+
+    /*
+
+//https://leetcode.com/problems/count-sorted-vowel-strings/discuss/918498/JavaC%2B%2BPython-DP-O(1)-Time-and-Space
+
+     int countVowelStrings(int n) {
+
+         vector<int> dp={0,1,1,1,1,1};
+
+         for( int i=1; i<=n ; i++){
+
+             for( int j=1; j<=5; j++){
+                 dp[j]+= dp[j-1];
+             }
+         }
+         return dp[5];
+    }
+
+    //its a dp qn with dp[i][k - 1] + dp[i - 1][k];
+
+
+     */
+
+}
